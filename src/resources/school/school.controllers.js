b7165b556f8f253cb5dbdd71e6338478296f3a26
Eric Kim <erickim195@gmail.com>
2021-06-25 15:56:50 -0700

    moved school.crud into utils/crud

--- src/resources/school/school.controllers.js
+++ src/resources/school/school.controllers.js
@@ -1,4 +1,4 @@
-import { schoolCrudControllers } from '../../utils/school.crud';
+import { schoolCrudControllers } from '../../utils/crud/school.crud';
 import { School } from './school.model';
 
 export default schoolCrudControllers(School);
